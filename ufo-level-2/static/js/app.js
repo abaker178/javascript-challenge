39415c7259ac19ce980eb85dc131a5c9a87dd214
Andrew Baker <abaker178@gmail.com>
2021-03-06 12:44:03 -0500

    cleaned up level 1 and level 2 directories

--- ufo-level-2/static/js/app.js
+++ ufo-level-2/static/js/app.js
@@ -1,11 +1,27 @@
 // from data.js
 const _tableData = data;
 
+// table fields
+const _inputIDs = ["datetime", "city", "state", "country", "shape"];
+
 // copy the table data to the filtered table
 var filteredTable = _tableData;
 
 // select the table 
 var table = d3.select("#ufo-table>tbody");
+
+// generate options for shape field
+shapeSet = new Set();
+_tableData.forEach(function(entry) {
+    Object.entries(entry).forEach(([key, value]) => {
+        if (key == "shape") {
+            shapeSet.add(value);
+        }
+})});
+// create shape options list
+let shapeElem = d3.select("#shape");
+shapeElem.append("option").property("selected","selected").text("");
+shapeSet.forEach(shape => shapeElem.append("option").property("value", shape).text(shape));
 
 // table creation function
 function createRow(entry) {
@@ -30,10 +46,12 @@ function filter() {
     reset();
 
     // filter based on user inputs
-    let userInput = d3.select(`#datetime`).property("value");
-    if (userInput !== "") {
-        filteredTable = filteredTable.filter(entry => entry.datetime === userInput);
-    }
+    _inputIDs.forEach(function(id){
+        let userInput = d3.select(`#${id}`).property("value");
+        if (userInput !== "") {
+            filteredTable = filteredTable.filter(entry => entry[id] === userInput.toLowerCase());
+        }
+    });
 
     // check to see if there are any values that
     // will show based on the search criteria
@@ -46,9 +64,15 @@ function filter() {
     }
 }
 
+// reset the search table
+function resetSearch() {
+    _inputIDs.forEach(id => d3.select(`#${id}`).property("value", ""));
+}
+
 // loop and build the table
 filteredTable.forEach(createRow);
 
 // add event handlers
 d3.select("form").on("submit", filter);
 d3.select("#filter-btn").on("click", filter);
+d3.select("#reset-btn").on("click", resetSearch);
